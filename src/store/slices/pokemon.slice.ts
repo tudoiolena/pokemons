c8efb2e633852b007b253239274d204ce3979cf2
Olena Tudoi <olena.tudoi@gmail.com>
2024-03-26 23:15:55 +0200

    UPD: Update pokemon slice state

--- src/store/slices/pokemon.slice.ts
+++ src/store/slices/pokemon.slice.ts
@@ -5,11 +5,15 @@ import { Pokemon } from "../../types/pokemon-response";
 interface PokemonState {
   pokemons: PokemonListResponse | [];
   selectedPokemon: Pokemon | null;
+  isLoading: boolean;
+  error: string | null;
 }
 
 const initialState: PokemonState = {
   pokemons: [],
   selectedPokemon: null,
+  isLoading: false,
+  error: null,
 };
 
 const pokemonSlice = createSlice({
@@ -18,11 +22,31 @@ const pokemonSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
+      .addCase(fetchPokemons.pending, (state) => {
+        state.isLoading = true;
+      })
       .addCase(fetchPokemons.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
         state.pokemons = action.payload;
+      })
+      .addCase(fetchPokemons.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error
+          ? action.error.message!
+          : "An error occurred";
+      })
+      .addCase(fetchPokemonDetails.pending, (state) => {
+        state.isLoading = true;
       })
       .addCase(fetchPokemonDetails.fulfilled, (state, action) => {
         state.selectedPokemon = action.payload;
+      })
+      .addCase(fetchPokemonDetails.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error
+          ? action.error.message!
+          : "An error occurred";
       });
   },
 });
