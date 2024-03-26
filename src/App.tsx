5b02090b7afcaaa786ae8d84e6634f0fe8f61214
Olena Tudoi <olena.tudoi@gmail.com>
2024-03-26 15:09:21 +0200

    NEW: Add render pokemon details

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch } from "./store/store";
-import { PokemonsList } from "./store/slices/pockemon.slice";
-import { fetchPokemons } from "./store/actions";
+import {
+  PokemonsList,
+  selectedPokemonData,
+} from "./store/slices/pockemon.slice";
+import { fetchPokemonDetails, fetchPokemons } from "./store/actions";
+import { PokemonDetails } from "./components/pockemon-details";
 
 function App() {
   const dispatch = useDispatch<AppDispatch>();
   const pokemons = useSelector(PokemonsList);
+  const selectedPokemon = useSelector(selectedPokemonData);
+  const [clickedPokemon, setClickedPokemon] = useState(null);
+
+  const handlePokemonClick = (pokemonName) => {
+    dispatch(fetchPokemonDetails(pokemonName));
+    setClickedPokemon(pokemonName);
+  };
 
   useEffect(() => {
     dispatch(fetchPokemons());
   }, [dispatch]);
 
   return (
-    <>
-      <div>
-        {pokemons &&
-          pokemons.map((pokemon, index) => <li key={index}>{pokemon.name}</li>)}
+    <div>
+      <div className="pokemon-list">
+        <h2>Pokemons</h2>
+        <ul>
+          {pokemons &&
+            pokemons.map((pokemon, index) => (
+              <li key={index} onClick={() => handlePokemonClick(pokemon.name)}>
+                {pokemon.name}
+              </li>
+            ))}
+        </ul>
       </div>
-    </>
+      {selectedPokemon && (
+        <div>
+          <h2>Pokemon Details</h2>
+          <PokemonDetails pokemon={selectedPokemon} />
+        </div>
+      )}
+    </div>
   );
 }
 
---
+++ src/components/pockemon-details.tsx
@@ -0,0 +1,31 @@
+export const PokemonDetails = ({ pokemon }) => {
+  if (!pokemon) return null;
+  const { name, abilities, sprites, base_experience, height, weight, moves } =
+    pokemon;
+  console.log(pokemon);
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <img src={sprites.front_default} alt={name} />
+      <h3>Abilities:</h3>
+      <ul>
+        {abilities &&
+          abilities.map((ability, index) => (
+            <li key={index}>{ability.ability.name}</li>
+          ))}
+      </ul>
+      <h3>Characteristics:</h3>
+      <ul>
+        <li>Base experience {base_experience}</li>
+        <li>Height {height}</li>
+        <li>Weight {weight}</li>
+        <h3>Moves:</h3>
+        {moves &&
+          moves.map((moveData, index) => (
+            <li key={index}>{moveData.move.name}</li>
+          ))}
+      </ul>
+    </div>
+  );
+};
